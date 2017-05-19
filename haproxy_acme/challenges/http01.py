49c840a161ad129f16d67bc2a30fbbb1f3e9f0b1
Martijn Braam <martijn@brixit.nl>
2017-05-19 22:21:25 +0200

    Fixed file write

--- haproxy_acme/challenges/http01.py
+++ haproxy_acme/challenges/http01.py
@@ -6,7 +6,7 @@ def process_http01_challenge(challenge, thumbprint, directory):
     token = re.sub(r"[^A-Za-z0-9_\-]", "_", challenge['token'])
     keyauthorization = "{}.{}".format(token, thumbprint)
 
-    with open(os.path.join(directory, token)) as handle:
+    with open(os.path.join(directory, token), 'w') as handle:
         handle.write(keyauthorization)
 
     return keyauthorization
